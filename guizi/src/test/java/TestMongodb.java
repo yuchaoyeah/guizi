35996aaeb4d39463d225be553370784007e5a28d
xiol <xionglin1900@126.com>
2016-12-18 23:05:15 +0800

    just change its name

---
+++ guizi/src/test/java/TestMongodb.java
@@ -0,0 +1,35 @@
+
+
+import org.junit.Test;
+
+import guizi.user.dao.UserDao;
+import guizi.user.entity.User;
+
+public class TestMongodb{
+
+	@Test
+	public void test() {
+		/*User user = new User();
+		user.setName("delores");
+		user.setAge(20);
+		user.setGender(false);*/
+		
+		UserDao userDao = new UserDao();
+		
+		/*User me = new User();
+		me.setName("voyager");
+		me.setAge(24);
+		me.setGender(true);
+		userDao.save(me);*/
+		
+		User user = userDao.get("name", "voyager");
+		if(user != null) {
+			user.setAge(26);
+			userDao.save(user);
+			
+			user = userDao.get("name", "voyager");
+		}
+		
+		System.out.println(user.toString());
+	}
+}
